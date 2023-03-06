5d40cfbb7b65bcdce4843fb8cf7522309f94b3ee
njorokel <kelvinmkoloni17@gmail.com>
2023-03-06 15:47:52 +0300

    a story

---
+++ app.py
@@ -0,0 +1,12 @@
+name1='kelvin'
+name2='jane'
+verb1='swimming'
+noun1='river'
+verb2='changed'
+verb3='jumped'
+adjective1='cold'
+adjective2='relaxing'
+adjective3='enormous'
+noun2='monkey'
+noun3='clothes'
+print("one day " +name1+ " and " +name2+ " went " +verb1+ " in the " +noun1+ " they " +verb2+ " and " +verb3+ " into the " +adjective1+ " water.the water was so " +adjective2+ " when " +verb1+ " a group of " +adjective3+ " " +noun2+ " stole their " +noun3+ " and ran away.")
